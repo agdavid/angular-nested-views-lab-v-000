b0984e4e08ec6d4c16695a280dfddf5c91abb03f
agdavid <antonio.g.david@gmail.com>
2016-09-13 14:00:58 -0400

    add nested routes to ui.router in root module

--- js/app/app.js
+++ js/app/app.js
@@ -1,2 +1,22 @@
+function Router($stateProvider) {
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: 'views/home.html'
+    })
+    .state('home.timeline', {
+      url: '/timeline',
+      templateUrl: 'views/home/timeline.html'
+    })
+    .state('home.notifications', {
+      url: '/notifications',
+      templateUrl: 'views/home/notifications.html'
+    })
+    .state('home.profile', {
+      url: '/profile',
+      templateUrl: 'views/home/user.html'
+    })
+}
 angular
-    .module('app', ['']);
+    .module('app', ['ui.router'])
+    .config(Router);
